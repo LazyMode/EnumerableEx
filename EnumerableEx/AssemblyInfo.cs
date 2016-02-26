dd80fbf4ed7a5c4a7945b433d48f02f85fceed37
Licshee <licshee@yen.ai>
2016-02-26 15:21:38 +0800

    .

--- EnumerableEx/AssemblyInfo.cs
+++ EnumerableEx/AssemblyInfo.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Reflection;
+﻿using System.Reflection;
 
 [assembly: AssemblyVersion("0.1")]
 [assembly: AssemblyInformationalVersion("0.1-Algos")]
-
